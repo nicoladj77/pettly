25dff9ba11f50e0c1b480955b2b1b4a2f899ca8d
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2014-08-06 18:31:24 +0200

    add fields to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806125904) do
+ActiveRecord::Schema.define(version: 20140806155511) do
 
   create_table "photos", force: true do |t|
     t.string   "image_uid"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 20140806125904) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.integer  "role"
+    t.string   "username"
+    t.string   "surname"
+    t.date     "date_of_birth"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
